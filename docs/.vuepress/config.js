131d24385eba9cdb0d4b22299c93c021d0dcac9d
cgh_yc <13049617353@163.com>
2020-12-14 21:54:51 +0800

    1214

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'cgh_yc的博客',
+  description: '专注前端技术栈分享',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Html', link: '/html/' },
+      { text: 'Css', link: '/css/' },
+      { text: 'JavaScript', link: '/javascript/' },
+      { text: '面试问题', 
+        items: [
+          { text: '笔试', link: '/interview/written_examination' },
+          { text: '面试', link: '/interview/interview_examination' },
+        ] 
+      },
+    ]
+  }
+}
+
